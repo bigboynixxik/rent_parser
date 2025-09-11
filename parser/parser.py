f5752bfcf06bfb79555956cdf6758a79a5ab4fbd
Nixxik <v.gorbachev05@yandex.ru>
2025-09-11 13:15:35 +0700

    Парсинг изображений

--- parser/parser.py
+++ parser/parser.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent as ua
 
 from settings import URL, DEAL_TYPE, MAX_PRICE, MIN_PRICE, OFFER_TYPE, ROOM1, \
-    ROOM2, ROOM3, PAGE, REGION
+    ROOM2, PAGE, REGION
 
 
 class Parser:
@@ -46,6 +46,10 @@ class Parser:
             time.sleep(3)
             for div in divs:
                 link = div.find('a')
+                images = link.find_all('img')
+                images_list = []
+                for img in images:
+                    images_list.append(img.get('src'))
                 information = div.find_all('div', attrs={
                     'data-name': 'GeneralInfoSectionRowComponent'})
                 flat_info = []
@@ -57,8 +61,10 @@ class Parser:
                         flat_info.append(some_text[0])
                     if some_text == ['']:
                         flat_info.append(link.get('href'))
+                        flat_info.append(images_list)
                         self.data.append(flat_info)
                         flat_info = []
+                images_list = []
         except Exception as e:
             print(e)
             time.sleep(1)
@@ -67,25 +73,34 @@ class Parser:
     def get_html(self):
         self._get_response()
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
+        time.sleep(5)
 
     def parsing(self):
         self.get_html()
         self.start_parsing()
 
     def work_with_data(self):
+        """
+        Метод для обработки данных, которые в последствии будут храниться
+        :return:
+        """
         flat_dict = {}
         for flat in self.data:
             flat_dict['flat_info'] = flat[0]
             flat_dict['flat_address'] = flat[1]
             flat_dict['flat_price'] = flat[2]
             flat_dict['flat_descript'] = flat[3]
-            flat_dict['flat_link'] = flat[-1]
+            flat_dict['flat_link'] = flat[-2]
+            flat_dict['flat_img'] = flat[-1]
             self.sorted_flat_data.append(flat_dict)
             flat_dict = {}
         for flat in self.sorted_flat_data:
             for key, value in flat.items():
                 print(f'{key} - {value}')
-            print('================================================================\n')
+            print(
+                '================================================================\n')
+
+
 parser = Parser()
 parser.parsing()
 parser.work_with_data()
